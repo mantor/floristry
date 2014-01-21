8e8003ec6cca05e5ec44313f6452f4836392274f
Danny Fullerton <northox@mantor.org>
2014-01-20 22:57:10 -0500

    Move ActiveParticipant to Ruote-Trail and remove ActiveParticipant stuff from Participant

---
+++ app/models/ruote_trail/active_participant.rb
@@ -0,0 +1,36 @@
+module RuoteTrail
+
+  # ActiveParticipant Leaf Expression - see lib/active_ruote/participant.rb for more
+  #
+  class ActiveParticipant < LeafExpression
+    attr_accessor :task
+
+    def initialize(id, name, params = {}, workitem = {}, era = :present)
+
+      super(id, name, params, workitem, era)
+      params_handler
+    end
+
+    def update_attributes(new_attributes, options={})
+
+      instance.update_attributes(new_attributes, options)
+    end
+
+    def errors() instance.errors end
+
+    def instance
+
+      @instance ||= @task.camelize.constantize.new(self)
+    end
+
+    protected
+
+    def params_handler
+
+      @task = params['task']
+
+      #match = task_param.match /\A(\w+)(\/(\w+))?(#(\w+))?\z/
+      #nada, component, model, nada, id = match.captures if match
+    end
+  end
+end
