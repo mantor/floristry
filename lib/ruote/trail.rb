d040392d333c4d28822274a77839df144c869798
Danny Fullerton <northox@mantor.org>
2014-04-29 10:17:19 -0400

    Fix require order

--- lib/ruote/trail.rb
+++ lib/ruote/trail.rb
@@ -4,13 +4,13 @@ require 'ruote/workitem'
 require 'ruote/trail/observer'
 require 'ruote/trail/configuration'
 require 'ruote/trail/engine'
-require 'ruote/trail/active_participant'
 
 require 'forwardable'
 require 'active_attr'
 require 'active_model'
 require 'active_model/mass_assignment_security'
 require 'active_model/mass_assignment_security/sanitizer'
+require 'ruote/trail/active_participant'
 
 module RuoteTrail
 end
