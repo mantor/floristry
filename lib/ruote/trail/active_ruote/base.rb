47f06c82cd3a3ccc09f821611b59ee03e62336c7
Danny Fullerton <northox@mantor.org>
2014-12-13 15:05:58 -0500

    Minor cleanup of ActiveRecord base

--- lib/ruote/trail/active_ruote/base.rb
+++ lib/ruote/trail/active_ruote/base.rb
@@ -6,7 +6,6 @@ module RuoteTrail::ActiveRuote
 
     after_initialize :default_values
 
-
     self.abstract_class = true
 
     # attr_accessible :__workitem__ # TODO should be a mixin??!?!
@@ -21,18 +20,21 @@ module RuoteTrail::ActiveRuote
       super(wi_h.keep_if {| key, value | self.column_names.include?(key) })
     end
 
-    # @TODO explain this
+    # ActiveRecords participants can be search by their Rails ID or Workflow id (feid)
+    #
+    # This is required since the information in hand from a Workflow perspective is always the
+    # Workflow ID, not it's Rails representation.
     #
     def self.find id
 
       if id.is_a? Integer
-        object = self.where(id: id).first
+        obj = self.where(id: id).first
       else
-        object = self.where(__feid__: id).first
+        obj = self.where(__feid__: id).first
       end
 
-      raise ActiveRecord::RecordNotFound unless object
-      object
+      raise ActiveRecord::RecordNotFound unless obj
+      obj
     end
 
     def wfid
@@ -45,10 +47,7 @@ module RuoteTrail::ActiveRuote
       'layouts/ruote_trail/leaf-expression'
     end
 
-    def image
-
-      false
-    end
+    def image() false end
 
     def active?()     @era == :present end
     def inactive?()   @era != :present end
@@ -85,13 +84,15 @@ module RuoteTrail::ActiveRuote
       receiver = RuoteTrail::ActiveRuote::Receiver.new(RuoteKit.engine)
       receiver.proceed(wi)
 
-      sleep(1) #@todo this sucks, but the trail seems to be written each time ruote 'steps' (@each 0.8s)
+      sleep(1) # TODO this sucks, but the trail seems to be written each time ruote 'steps' (@each 0.8s)
     end
 
     def merge_attributes_into_fields
+
       original_wi = JSON.parse(attributes['__workitem__']) # TODO error handling
-      new_attributes = attributes.reject { |key, value| ['id', '__workitem__', 'created_at', 'updated_at'].include? key }
+      new_attributes = attributes.reject { |key, value| %w(id __workitem__ created_at updated_at).include? key } # TODO centralize list
       original_wi['fields'] = original_wi['fields'].merge!(new_attributes)
+
       original_wi
     end
 
@@ -125,93 +126,7 @@ module RuoteTrail::ActiveRuote
     # end
 
     def proceed(workitem)
-
       reply(workitem)
     end
   end
-
-  # class Base # TODO should this be within Active::Participant ? First try failed.
-  #   include ActiveAttr::Model
-  #   include ActiveAttr::MassAssignment
-  #   include ActiveModel::MassAssignmentSecurity
-  #   extend Forwardable
-  #
-  #   def initialize(participant)
-  #
-  #     @participant = participant
-  #     load_attributes(workitem)
-  #   end
-  #
-  #   def_delegators :@participant, :active?, :inactive?, :is_past?, :is_present?, :is_future?,
-  #                  :id, :name, :params, :workitem, :era
-  #
-  #   # Assign instance variables and @attributes hash then save and proceed (if required) if valid.
-  #   #
-  #   def update_attributes(new_attributes, options={})
-  #
-  #     assign_attributes(new_attributes, options)
-  #     load_attributes(new_attributes)
-  #
-  #     save(options[:proceed]) if valid?
-  #   end
-  #
-  #   # Override default path # TODO really? look at the real to_partial_path maybe that's better
-  #   #
-  #   def to_partial_path
-  #
-  #     k = self.class.to_s.parameterize.underscore
-  #     "forms/tasks/#{k}/#{k}" # TODO is that really what we want? Segregated Components? Why?
-  #   end
-  #
-  #   # To make sure form_for use edit action instead of new
-  #   #
-  #   def persisted?() true end
-  #
-  #   protected
-  #
-  #   # Load attrs in @attributes hash if it's an attribute
-  #   #
-  #   # n.b. @attributes mimic ActiveRecord while attributes contains the list of ActiveAttr `virtual` attributes
-  #   #
-  #   def load_attributes(attrs)
-  #
-  #     @attributes ||= {}
-  #     attributes.each do |k, v|
-  #       #attr[k] ||= nil  # TODO needed? e.g. if not already set in the ruote's workitem
-  #       @attributes[k] = attrs[k]
-  #       self.write_attribute(k, attrs[k])
-  #     end
-  #   end
-  #
-  #   # Save workitem back in the workflow engine and proceed if needed
-  #   #
-  #   def save(proceed = false)
-  #
-  #     wi = to_ruote_wi
-  #     RuoteKit.storage_participant.do_update(wi)
-  #
-  #     if proceed
-  #       #wi['outcome'] = outcome             # TODO should be a state machine?
-  #       wi['exited_at'] = Ruote.now_to_utc_s # TODO get rid of this dependency
-  #
-  #       RuoteKit.storage_participant.proceed(wi)
-  #     end
-  #
-  #     true # TODO add validation or exception ?!
-  #   end
-  #
-  #   def to_ruote_wi
-  #
-  #     wi = RuoteKit.storage_participant[id]
-  #     attributes.each { |k, v| wi.set_field(k, v) }
-  #
-  #     wi
-  #   end
-  #
-  #   #def from_ruote_wi
-  #   #
-  #   #  TODO is the big case switch supposed to be here???
-  #   #end
-  # end
-
 end
