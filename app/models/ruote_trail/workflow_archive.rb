763a463d825bd79ae6ee3b4cfce108b624cdfce8
Jean-Francois Rioux <jeff@mantor.org>
2015-05-10 12:03:27 -0400

    Add a comment to explain the need for table_name.

--- app/models/ruote_trail/workflow_archive.rb
+++ app/models/ruote_trail/workflow_archive.rb
@@ -1,5 +1,8 @@
 module RuoteTrail
 class WorkflowArchive < ActiveRecord::Base
+  #Specify the table name, because the RuoteTrail module namespaces this class, and
+  #ActiveRecord table resolution/guess was expecting the table to be named:
+  # ruotetrail_workflow_archive
   self.table_name = 'workflow_archives'
   def trail=(t)
 
