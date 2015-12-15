7f6318757fae0877c520693b1ff25c23c13cffb4
Jean-Francois Rioux <jeff@mantor.org>
2015-12-14 22:40:00 -0500

    Delete notifications jobs when creating workflow archive.
    Related to OpenSec #66

--- lib/ruote/trail/archive/active_record.rb
+++ lib/ruote/trail/archive/active_record.rb
@@ -4,6 +4,13 @@ module Archive
   class ActiveRecord
     def self.archive(wf)
 
+      Delayed::Job.where("handler LIKE '%procedure%' AND handler LIKE :query", query: "%#{wf['wfid']}%").each { |job|
+        job.delete
+        # @todo move this out of here?
+        Rails.logger.info("Deleting JOB")
+        Rails.logger.info("#{job.inspect}")
+      }
+
       # wf['wfid'] = wf.delete 'id' # Don't override ActiveRecord surrogate key.
       wfa = WorkflowArchive.new(wf)
       wfa.save
