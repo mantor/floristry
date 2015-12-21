3f501081891e0a67ed1e06ecee109ae286322f40
Jean-Francois Rioux <jeff@mantor.org>
2015-12-20 22:31:47 -0500

    Remove Opensec specific items introduced in 69cb3f1

--- lib/generators/templates/participant.rb
+++ lib/generators/templates/participant.rb
@@ -1,7 +1,5 @@
 <% module_namespacing do -%>
 class <%= class_name %> < RuoteTrail::ActiveRecord::Base
-  has_one :participant_deadline, as: :timebound
-  delegate :due_date, to: :participant_deadline, :allow_nil => true
 <% attributes.select(&:reference?).each do |attribute| -%>
   belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %>
 <% end -%>
