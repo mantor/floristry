148343a9c6d0b59cdefbf42d578c3f4a06cb3036
Jean-Francois Rioux <jeff@mantor.org>
2016-10-26 20:05:42 -0400

    Add required method to have a consistent interface with WebParticipants.
    Related to #111

--- app/models/active_trail/participant.rb
+++ app/models/active_trail/participant.rb
@@ -6,7 +6,33 @@ module ActiveTrail
 
     def current_state
 
-      self.fields['current_state']
+      case era
+        when :present
+          'open'
+        when :past
+          'closed'
+        else
+          ''
+      end
+
+    end
+
+    def instance
+
+      self
+    end
+
+    def scope (s)
+
+      #TODO: how to deal my scope here ?? I don't have fields nor params.
+      s
+    end
+
+    def due_at
+
+      # TODO one day, this should be configured via web configs. Also in opensec/app/models/participant_deadline.rb
+      # Does this really matter in a context where we expect the process to run in an instant, e.g. SSH Participant??
+      Date.today
     end
   end
 end
