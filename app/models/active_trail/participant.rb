2270f73a70af7c482fc29b429d527308f71bff6f
Jean-Francois Rioux <jeff@mantor.org>
2016-10-30 17:27:51 -0400

    Remove scope method. It was really just returning anything that was passed to it. We dont need stupid stuff.

--- app/models/active_trail/participant.rb
+++ app/models/active_trail/participant.rb
@@ -22,16 +22,10 @@ module ActiveTrail
       self
     end
 
-    def scope (s)
-
-      #TODO: how to deal my scope here ?? I don't have fields nor params.
-      s
-    end
-
     def due_at
 
       # TODO one day, this should be configured via web configs. Also in opensec/app/models/participant_deadline.rb
-      # Does this really matter in a context where we expect the process to run in an instant, e.g. SSH Participant??
+      # Does this really matter here?? Participant that use this frontend handler should be code based and run in an instant.
       Date.today
     end
   end
