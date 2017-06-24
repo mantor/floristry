c01c81524026d816e98c1e69f2152607a4452a5f
Jean-Francois Rioux <jeff@mantor.org>
2017-06-24 12:51:29 -0400

    Prompts for flack and flor installation

--- lib/generators/active_trail/install_generator.rb
+++ lib/generators/active_trail/install_generator.rb
@@ -29,6 +29,23 @@ module ActiveTrail
       route("mount ActiveTrail::Engine => '/trail'")
     end
 
+    def install_flor_and_flack
+
+      r = yes?("\n- Would you like me to install Flack and Flor inside: #{File.expand_path("..", Dir.pwd)}\n (If you are installing the dummy test app, type yes) [Y/n]")
+      if r
+
+        inside('../') do
+
+          run('git clone https://github.com/floraison/flor && cd flor && git checkout v0.14.0')
+          run('git clone https://github.com/floraison/flack')
+        end
+
+        say("Copying default Flack hooks and taskers inside ../flack/envs/dev/lib/")
+        directory("flack/lib/hooks", "../flack/env/dev/lib/")
+        directory("flack/lib/taskers", "../flack/env/dev/lib/")
+      end
+    end
+
     def self.next_migration_number path
 
       if @previous_stamp.nil?
