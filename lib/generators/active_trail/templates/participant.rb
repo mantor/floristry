da4a87d25e96e36d8a3378e2b91fa75295f2bf89
Jean-Francois Rioux <jeff@mantor.org>
2017-10-08 16:32:40 -0400

    Fixes ActiveRecord backed partipants gen

--- lib/generators/active_trail/templates/participant.rb
+++ lib/generators/active_trail/templates/participant.rb
@@ -1,9 +1,7 @@
-module_namespacing do
-  class class_name < ActiveTrail::ActiveRecord::Base
-     attributes.select(&:reference?).each do |attribute|
-      belongs_to : attribute.name, polymorphic: true if attribute.polymorphic?
-    end
-    has_one :participant_deadline, as: :timebound
-    delegate :due_at, to: :participant_deadline, :allow_nil => true
-  end
+<% module_namespacing do -%>
+class <%= class_name %> < ActiveTrail::ActiveRecord::Base
+<% attributes.select(&:reference?).each do |attribute| -%>
+  belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %><%= ', required: true' if attribute.required? %>
+<% end -%>
 end
+<% end -%>
