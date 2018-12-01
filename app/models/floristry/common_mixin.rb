c4ba29712a07854e8c89bd391963818b0d744bdc
Jean-Francois Rioux <jeff@mantor.org>
2018-12-01 16:20:33 -0500

    Pair programming session
    - Align naming with Flor
    - Cleanup todos
    - Obliterate commented out code
    - Documentation

--- app/models/floristry/common_mixin.rb
+++ app/models/floristry/common_mixin.rb
@@ -1,14 +1,14 @@
 module Floristry::CommonMixin
 
   CHILDREN = 1     # Where branch expressions stores children expressions
-  ROOT_EXPID = '0' # Root expression id - yes, it's a string (e.g. 0_1_0)
-  SEP = '!'        # FEID's field separator
-  NID_SEP = '_'    # Nid separator
-  FEID_REGEX = /\A([\w\.\-]+)!?([0-9_]+)?\z/ # domain0-u0-20170806.2124.pufatsonaju!0_1
+  ROOT_NID = '0'   # Root expression id - yes, it's a string (e.g. 0_1_0)
+  FEI_SEP = '!'    # FEI separator 
+  NID_SEP = '_'    # NID separator
+  FEI_REGEX = /\A([\w\.\-]+)!?([0-9_]+)?\z/ # domain0-u0-20170806.2124.pufatsonaju!0_1
 
-  attr_reader :engineid, :exid, :subid, :nid
+  attr_reader :exid, :nid
 
-  delegate :engineid, :exid, :nid, to: :@fei
+  delegate :exid, :nid, to: :@fei
 
   def self.included(base)
 
@@ -18,94 +18,55 @@ module Floristry::CommonMixin
 
   module InstanceMethods
 
-    def to_wfid(feid) feid.split(SEP).last end
+    def to_nid(fei) fei.split(FEI_SEP).last end
 
-    def to_expid(feid) feid.split(SEP).first end
+    def to_exid(fei) fei.split(FEI_SEP).first end
 
-    # The FlowExpressionId (fei for short) is an process expression identifier.
-    # Each expression when instantiated gets a unique fei.
+    # The FlowExpressionId (fei for short) is an flow identifier.
     #
-    # Feis are also used in msgs, where the fei is the fei of the
-    # [participant] expression that emitted the msg.
-    #
-    # Feis can thus indicate the position of a msg in a process tree.
-    #
-    # Feis contain four pieces of information :
-    #
-    # * wfid : workflow instance id, the identifier for the process instance
-    # * subid : a unique identifier for expressions (useful in loops)
-    # * expid : the expression id, where in the process tree
-    # * engine_id : only relevant in multi engine scenario (defaults to 'engine')
+    # FEI contain two pieces of information :
+    # * exid : execution instance id, the identifier for the flow instance
+    # * nid : the node id - the position within the flow 
     #
     class FlowExpressionId
 
-      attr_accessor :id, :exid, :nid, :feid
+      attr_accessor :id, :exid, :nid
 
       def initialize(id)
-
-        # todo we should have exid -> Flor execution id -> For the whole flow : replaces wfid
-        # todo                expid -> combination of exid and nid : replaces feid
-        # todo                domain ?
+        
         @focus = true
         @id = id
-        if id.is_a?(String) && id.match(FEID_REGEX)
+        if id.is_a?(String) && id.match(FEI_REGEX)
 
-          s = id.split(SEP)
+          s = id.split(FEI_SEP)
 
           if s.size > 1
-            @feid = id
             @exid = s[-2]
-            @nid = s[-1] || NO_SUBID
+            @nid = s[-1]
           else
             @exid = s[-1]
           end
-
-          # @id = to_feid
-
-        # elsif id.is_a?(Hash) && id.has_key?(:wfid)
-        #
-        #   @engineid = id[:engine_id] || 'engine'
-        #   @expid = id[:expid] || default_expid
-        #   @subid = id[:subid] || NO_SUBID
-        #   @wfid = id[:wfid]
-        #   @id = to_feid
         else
 
-          raise ActiveRecord::RecordNotFound
+          raise ActiveRecord::RecordNotFound # TODO not the right class of exception ( invalidrequest, parameter)
         end
       end
 
-      def focussed?() @focus end # Identifies whether a specific expid was requested
-
-      def to_feid(opts = {})
-
-        @id
-        # expid = opts.include?(:expid) ? opts[:expid] : @expid
-        # subid = opts.include?(:no_subid) ? NO_SUBID  : @subid
-        #
-        # [ expid, subid, @wfid ].join(SEP)
-      end
-
-      protected
-
-      def default_expid
-
-        @focus = false
-        ROOT_EXPID
-      end
+      def focussed?() @focus end # Identifies whether a specific nid was requested
+      
     end
   end
 
   module ClassMethods
 
-    def to_wfid(feid)
+    def to_nid(fei)
 
-      feid.split(SEP).last
+      fei.split(FEI_SEP).last
     end
 
-    def to_expid(feid)
+    def to_exid(fei)
 
-      feid.split(SEP).first
+      fei.split(FEI_SEP).first
     end
   end
 end
