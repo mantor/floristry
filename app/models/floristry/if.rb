4925e254c1003d7bb2c07a4092f40d9dcb9fca7c
Jean-François Rioux <jeff@mantor.org>
2021-02-20 10:05:29 -0500

    Add comparison to flo_if (#63)
    * If condition rendering.
    Related to #37
    
    * Remove except clause from payload rendering in FloTask view.

--- app/models/floristry/if.rb
+++ app/models/floristry/if.rb
@@ -1,5 +1,25 @@
 module Floristry
   class If < BranchProcedure
 
+    def comparison
+
+      atts = children[0].raw_atts[0]
+      comparator = atts[0]
+      left = comparison_value(atts[1][0][1])
+      right = comparison_value(atts[1][1][1])
+
+      "if (#{left} #{comparator} #{right})"
+    end
+
+    private
+
+    def comparison_value(att)
+
+      comp_val = att
+      if comp_val.is_a? Array
+        comp_val = "#{comp_val[0][1]}.#{comp_val[1][1]}"
+      end
+      comp_val
+    end
   end
 end
